6d4f32ad7f12704e3beededb2c5047c589d3add1
Coder692 <beep690@gmail.com>
2015-03-02 23:09:39 -0800

    Shoot arrow
    Shoots an arrow when spacebar is pressed

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public GameObject BulletPrefab;
+
+public class ArrowScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		
+	if (Input.GetKeyDown(KeyCode.Space))
+	{
+		Instantiate(BulletPrefab, transform.position, transform.rotation);
+	}
+	
+	}
+}
